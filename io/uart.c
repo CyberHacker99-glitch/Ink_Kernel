b292b86b123e67f57067c823f9fd2d5c83500a0e
CyberHacker99-glitch <abhaypratapsinghsengar024@outlook.com>
2026-04-27 13:55:02 +0530

    Implement UART character and string output functions

--- io/uart.c
+++ io/uart.c
@@ -1,1 +1,13 @@
+#include "uart.h"
 
+#define UART0 ((volatile char*)0x10000000)
+
+void uart_putc(char c) {
+    *UART0 = c;
+}
+
+void uart_puts(const char *str) {
+    while (*str) {
+        uart_putc(*str++);
+    }
+}
